80a2f05fbaed20f82b9dfcedac6f3cba07e6912f
malvin99 <41668863+malvin99@users.noreply.github.com>
2023-10-21 09:42:42 +0200

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "{Insert your own AWS API Gateway Endpoint URL here}",
+                url: "{https://zjiutci6n6.execute-api.eu-west-2.amazonaws.com/default/LambdaFunction_Malvin}",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
