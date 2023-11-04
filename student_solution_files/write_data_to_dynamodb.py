1b8cfc19e548c63a7a0d9ab5b9f235b46a02e062
malvin99 <41668863+malvin99@users.noreply.github.com>
2023-11-04 05:58:44 +0200

    Fixed

--- student_solution_files/write_data_to_dynamodb.py
+++ student_solution_files/write_data_to_dynamodb.py
@@ -54,10 +54,10 @@ def lambda_handler(event, context):
     # incoming data is sent to the matching column in your DynamoDB table
     # --- Insert your code here ---
     db_response = table.put_item(Item={'ResponsesID': rid, # <--- Insert the correct variable
-                        'Name': data["name"], # <--- Insert the correct variable
-                        'Email': data["email"], # <--- Insert the correct variable
-                        'Cell': data["phone"], # <--- Insert the correct variable
-                        'Message': data["message"] # <--- Insert the correct variable
+                        'Name': dec_dict["name"], # <--- Insert the correct variable
+                        'Email': dec_dict["email"], # <--- Insert the correct variable
+                        'Cell': dec_dict["phone"], # <--- Insert the correct variable
+                        'Message': dec_dict["message"] # <--- Insert the correct variable
     })
     # -----------------------------
 
